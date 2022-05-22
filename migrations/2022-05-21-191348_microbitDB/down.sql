115115fce26601be4e3d4f255b7ec8553cc6f291
Charlie Cohen <charlie11cohen@gmail.com>
2022-05-22 20:31:00 +0100

    latest update

--- migrations/2022-05-21-191348_microbitDB/down.sql
+++ migrations/2022-05-21-191348_microbitDB/down.sql
@@ -1,1 +1,3 @@
 -- This file should undo anything in `up.sql`
+ DROP TABLE IF EXISTS public."Microbits" CASCADE;
+ DROP TABLE IF EXISTS public."Users" CASCADE;
--- migrations/2022-05-21-191348_microbitDB/up.sql
+++ migrations/2022-05-21-191348_microbitDB/up.sql
@@ -1,50 +1,25 @@
 -- Your SQL goes here
--- Table: public.Microbits
-
--- DROP TABLE IF EXISTS public."Microbits";
-
-CREATE TABLE IF NOT EXISTS public."Microbits"
+CREATE TABLE public."Users"
 (
-    id text COLLATE pg_catalog."default" NOT NULL,
+    id serial NOT NULL,
+    email text NOT NULL,
+    password text NOT NULL,
+    PRIMARY KEY (id)
+);
+
+
+    CREATE TABLE public."Microbits"
+(
+    id text NOT NULL,
     user_id integer,
     active_begin_hours integer NOT NULL DEFAULT 0,
     active_begin_minutes integer NOT NULL DEFAULT 0,
     active_end_hours integer NOT NULL DEFAULT 0,
     active_end_minutes integer NOT NULL DEFAULT 0,
-    CONSTRAINT "Microbits_pkey" PRIMARY KEY (id),
-    CONSTRAINT owner FOREIGN KEY (user_id)
+    PRIMARY KEY (id),
+    CONSTRAINT "Owner" FOREIGN KEY (user_id)
         REFERENCES public."Users" (id) MATCH SIMPLE
         ON UPDATE RESTRICT
         ON DELETE RESTRICT
         NOT VALID
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public."Microbits"
-    OWNER to postgres;
--- Index: fki_owner
-
--- DROP INDEX IF EXISTS public.fki_owner;
-
-CREATE INDEX IF NOT EXISTS fki_owner
-    ON public."Microbits" USING btree
-    (user_id ASC NULLS LAST)
-    TABLESPACE pg_default;
-
-    -- Table: public.Users
-
--- DROP TABLE IF EXISTS public."Users";
-
-CREATE TABLE IF NOT EXISTS public."Users"
-(
-    id integer NOT NULL DEFAULT nextval('"Users_id_seq"'::regclass),
-    email text COLLATE pg_catalog."default" NOT NULL,
-    password text COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT "Users_pkey" PRIMARY KEY (id)
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public."Users"
-    OWNER to postgres;
+);
